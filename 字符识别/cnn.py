2d31d423bb9608abd8a087913aa127d29e2c7e60
Kaixuan <lxk686868@outlook.com>
2022-07-15 01:17:37 +0800

    add predict module

--- 字符识别/cnn.py
+++ 字符识别/cnn.py
@@ -4,25 +4,25 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import models, layers
 
-
 TRAIN_DIR = "./enu_train"
 TEST_DIR = "./enu_test"
+MODEL_PATH = "./model/model.h5"
 IMAGE_WIDTH = 20
 IMAGE_HEIGHT = 20
 CLASSIFICATION_COUNT = 34
 LABEL_DICT = {
-	'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,
-	'A':10, 'B':11, 'C':12, 'D':13, 'E':14, 'F':15, 'G':16, 'H':17,	'J':18, 'K':19,
-	'L':20, 'M':21, 'N':22, 'P':23, 'Q':24, 'R':25, 'S':26, 'T':27, 'U':28, 'V':29,
-	'W':30, 'X':31, 'Y':32, 'Z':33
+    '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+    'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15, 'G': 16, 'H': 17, 'J': 18, 'K': 19,
+    'L': 20, 'M': 21, 'N': 22, 'P': 23, 'Q': 24, 'R': 25, 'S': 26, 'T': 27, 'U': 28, 'V': 29,
+    'W': 30, 'X': 31, 'Y': 32, 'Z': 33
 }
 
 physical_devices = tf.config.list_physical_devices('GPU')
 try:
-  tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 except:
-  # Invalid device or cannot modify virtual devices once initialized.
-  pass
+    # Invalid device or cannot modify virtual devices once initialized.
+    pass
 
 
 class Cnn(object):
@@ -30,11 +30,11 @@ class Cnn(object):
     CNN网络
     """
 
-    def __init__(self) -> None:
+    def __init__(self):
         pass
 
-    def build_model(self) -> None:
-        print('搭建网络...')
+    def build_model(self):
+        print('build model...')
         self.model = models.Sequential()
 
         self.model.add(layers.Conv2D(
@@ -71,7 +71,7 @@ class Cnn(object):
     #     self.train_labels = train_labels
     #     self.test_labels = test_labels
 
-    def load_data(self,dir_path):
+    def load_data(self, dir_path):
         data = []
         labels = []
 
@@ -87,55 +87,53 @@ class Cnn(object):
 
         return np.array(data), np.array(labels)
 
-    def onehot_labels(self,labels):
+    def onehot_labels(self, labels):
         onehots = np.zeros((len(labels), CLASSIFICATION_COUNT))
         for i in np.arange(len(labels)):
             onehots[i, labels[i]] = 1
         return onehots
 
-    def preprocess(self):
-        train_data,train_labels=self.load_data(TRAIN_DIR)
-        test_data,test_labels=self.load_data(TEST_DIR)
-        train_data =(train_data-train_data.mean())/train_data.max()
-        test_data =(test_data-test_data.mean())/test_data.max()
-        train_labels=self.onehot_labels(train_labels)
-        test_labels=self.onehot_labels(test_labels)
-        train_data=tf.reshape(train_data,[-1,IMAGE_HEIGHT,IMAGE_WIDTH,1])
-        test_data=tf.reshape(test_data,[-1,IMAGE_HEIGHT,IMAGE_WIDTH,1])
+    def preprocess_data(self):
+        train_data, train_labels = self.load_data(TRAIN_DIR)
+        test_data, test_labels = self.load_data(TEST_DIR)
+        train_data = (train_data - train_data.mean()) / train_data.max()
+        test_data = (test_data - test_data.mean()) / test_data.max()
+        train_labels = self.onehot_labels(train_labels)
+        test_labels = self.onehot_labels(test_labels)
+        train_data = tf.reshape(train_data, [-1, IMAGE_HEIGHT, IMAGE_WIDTH, 1])
+        test_data = tf.reshape(test_data, [-1, IMAGE_HEIGHT, IMAGE_WIDTH, 1])
 
-        self.train_images=train_data
-        self.train_labels=train_labels
-        self.test_images=test_data
-        self.test_labels=test_labels
-        print('train_lables.shape(%s)' % str(self.train_labels.shape))
-        print('loading data')
+        self.train_images = train_data
+        self.train_labels = train_labels
+        self.test_images = test_data
+        self.test_labels = test_labels
+        # print('train_lables.shape(%s)' % str(self.train_labels.shape))
+        print('loading data...')
 
-
-
-    def train(self, epoch=40) -> None:
-        print('训练模型...')
+    def train(self, epoch=40):
+        print('training...')
         self.model.fit(self.train_images,
                        self.train_labels,
                        epochs=epoch
                        )
 
-    def evaluate(self) -> None:
-        print('评估模型...')
+    def evaluate(self):
+        print('evaluating...')
         self.model.evaluate(self.test_images, self.test_labels)
 
-    def save_model(self, model_path='./model/model.h5') -> None:
-        print('保存模型...')
+    def save_model(self, model_path=MODEL_PATH):
+        print('save model...')
         self.model.save(model_path)
 
-    def load_model(self, model_path='./model/model.h5') -> None:
-        print('读取模型...')
+    def load_model(self, model_path=MODEL_PATH):
+        print('load model...')
         self.model = models.load_model(model_path)
 
 
-if __name__ =='__main__':
-
-    cnn=Cnn()
-    cnn.preprocess()
+if __name__ == '__main__':
+    cnn = Cnn()
+    cnn.preprocess_data()
     cnn.build_model()
     cnn.train()
     cnn.evaluate()
+    cnn.save_model()
